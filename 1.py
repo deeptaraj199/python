a1dc1e64689d6814000b5813cac45fe88ad6ef01
deeptaraj199 <143480785+deeptaraj199@users.noreply.github.com>
2023-09-21 11:33:42 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+x = 20
+y = 30
+print("Before swap -> ",x, y)
+x,y = y,x
+print("After swap -> ",x, y)
---
+++ 2.py
@@ -0,0 +1,4 @@
+length = float(input("What is the length of rectangle?"))
+width = float(input("What is the width of rectangle?"))
+area = length * width
+print("The area of rectangle is:",area)
